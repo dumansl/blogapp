7175a7df58ff35779066636693499bbfb251f8ec
Şule Duman <sduman_816@hotmail.com>
2024-05-09 01:32:06 +0300

    Added ejs method

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const path = require("path");
+const data = {
+  title: "Popüler Kurslar",
+  categories: [
+    "Web Geliştirme",
+    "Programlama",
+    "Mobil Uygulama",
+    "Veri Analizi",
+    "Ofis Uygulamaları",
+  ],
+};
 
 router.use("/blogs/:blogid", function (req, res) {
-  res.sendFile(path.join(__dirname, "../views/users", "blog-details.html"));
+  res.render("users/blog-details");
 });
 
 router.use("/blogs", function (req, res) {
-  res.sendFile(path.join(__dirname, "../views/users", "blogs.html"));
+  res.render("users/blogs");
 });
 
 router.use("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../views/users", "index.html"));
+  res.render("users/index", data);
 });
 
 module.exports = router;
